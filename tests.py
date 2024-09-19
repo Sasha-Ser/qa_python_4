8f71c4c036a20684c1eaa42255239dc3d3001d89
“Sasha <sandy.sergeev@yandex.ru>
2024-09-19 21:12:08 +0300

    Add tests and description

--- tests.py
+++ tests.py
@@ -1,24 +1,89 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
+    @pytest.fixture(autouse=True)
+    def book_info(self):
+        self.book = "Мастер и Маргарита"
+        self.genre = "Фантастика"
+        return self.book
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_add_new_book_add_book_true(self):  #добавление новой книги в словарь books_genre
         collector = BooksCollector()
+        collector.add_new_book(self.book)
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert self.book in collector.get_books_genre()
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_too_long_name_of_book_true(self):  # добавление новой книги в словарь books_genre с названием в 41 симвоо
+        collector = BooksCollector()
+        collector.add_new_book('Тайны Вселенной: путешествие в мир знаний')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert not 'Тайны Вселенной: путешествие в мир знаний' in collector.get_books_genre()
+
+    def test_set_book_genre_set_fantastc_true(self):  #устанавливаем книге жанр
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, 'Фантастика')
+
+        assert collector.get_book_genre(self.book) == 'Фантастика'
+
+    def test_get_book_genre_master_and_margarita_true(self):  #получение жанра книги по названию
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+
+        assert collector.get_book_genre(self.book) == self.genre
+
+    def test_get_books_with_specific_genre_fantastic_true(self):  #получение списка книг опредленного жанра
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+
+        assert collector.get_books_with_specific_genre(self.genre) == [self.book]
+
+    def test_get_books_genre_true(self): #поолучение словаря books_genre
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+
+        assert collector.get_books_genre() == {self.book: self.genre}
+
+    def test_get_books_for_children_add_azbuka_true(self): #получение списка книг, подходящие детям
+        collector = BooksCollector()
+        collector.add_new_book('Азбука')
+        collector.set_book_genre('Азбука','Комедии')
+
+        assert collector.get_books_for_children() == ['Азбука']
+
+    def test_get_books_for_children_add_adult_genre_false(self):  # проверка не добавления книги со взрослым жанром
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.set_book_genre('1985', 'Ужасы')
+
+        assert not collector.get_books_for_children() == ['1984']
+
+    def test_add_book_in_favorites_add_master_and_margo_true(self): #добавляем книгу в избранное
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+        collector.add_book_in_favorites(self.book)
+
+        assert self.book in collector.favorites
+
+    def test_delete_book_from_favorites_delete_master_and_margo_true(self): #удаление книги из избранного
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+        collector.add_book_in_favorites(self.book)
+        collector.delete_book_from_favorites(self.book)
+
+        assert self.book not in collector.favorites
+
+    def test_get_list_of_favorites_books_shows_master_and_margo_in_list_true(self): # получение списка избранного
+        collector = BooksCollector()
+        collector.add_new_book(self.book)
+        collector.set_book_genre(self.book, self.genre)
+        collector.add_book_in_favorites(self.book)
+
+        assert collector.favorites == [self.book]
